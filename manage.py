66ca1d5ae6ab1a582ff98e19263d19874819c469
Dellenoam <d.spiridonov.0256@gmail.com>
2024-03-15 13:13:22 +0500

    refactor: Refactor manage.py using Click library

--- manage.py
+++ manage.py
@@ -1,38 +1,37 @@
-import argparse
-import asyncio
+import click
 import sys
 import os
 
 
-async def main():
+@click.group()
+def main():
     """
-    A function to start the server and create a user using the argparse module.
-    It defines sub-parsers for 'runserver' and 'createuser' commands with corresponding arguments.
-    It then parses the arguments and executes the selected command.
+    Script to start the server and create a user
     """
-    parser = argparse.ArgumentParser(
-        description="Script to start the server and create a user"
-    )
-    subparsers = parser.add_subparsers(dest="command")
 
-    # Sub-parser for 'runserver' command
-    runserver_parser = subparsers.add_parser("runserver")
-    runserver_parser.add_argument("--port", type=int, default=8000)
-    runserver_parser.add_argument("--host", default="localhost")
 
-    # Sub-parser for 'createuser' command
-    subparsers.add_parser("createuser", help="Create a user")
+@main.command()
+@click.option("--port", type=int, default=8000, help="Port number to run the server on (default: 8000)")
+@click.option("--host", default="localhost", help="Host address to run the server on (default: localhost)")
+def runserver(port: int, host: str):
+    """
+    Start the server
+    """
+    import uvicorn
 
-    args = parser.parse_args()
+    uvicorn.run("src.main:app", host=host, port=port, reload=True)
 
-    if args.command == "runserver":
-        import uvicorn
-        uvicorn.run("src.main:app", host=args.host, port=args.port, reload=True)
-    elif args.command == "createuser":
-        from auth.management import prepare_user_creation
-        await prepare_user_creation()
+
+@main.command()
+async def createuser():
+    """
+    Create a user
+    """
+    from auth.management import prepare_user_creation
+
+    await prepare_user_creation()
 
 
 if __name__ == "__main__":
     sys.path.append(os.path.join(sys.path[0], "src"))
-    asyncio.run(main())
+    main()
